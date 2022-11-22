14d16f197fa233c62322ff351f08dbfb6c7cb4fd
Stefano Amenta <ame@softechweb.it>
2022-11-22 10:12:43 +0100

    è possibile selezionare i gruppi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectOptionsGroupsComponent } from './components/select-options-groups/select-options-groups.component';
 import { DemoMaterialModule } from 'src/modules/material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { DemoMaterialModule } from 'src/modules/material-module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DemoMaterialModule
+    DemoMaterialModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [SelectOptionsGroupsComponent],
   bootstrap: [AppComponent]
--- src/app/components/select-options-groups/select-options-groups.component.ts
+++ src/app/components/select-options-groups/select-options-groups.component.ts
@@ -7,14 +7,169 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./select-options-groups.component.scss']
 })
 export class SelectOptionsGroupsComponent implements OnInit {
-  isExpandCategory: boolean[] = [];
-  states = new FormControl();
+
+  // #region tendina 1
 
   foods = [
     {value: 'steak-0', viewValue: 'Steak'},
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
+
+  // #endregion
+
+  // #region tendina 2
+  isExpandCategory: boolean[] = [];
+  states = new FormControl();
+
+  expandDocumentTypes(group: any, groupIndex: number) {
+    console.log('expanding dropdown', group);
+    // this.isExpandCategory[group.groupLabel] =
+    //   !this.isExpandCategory[group.groupLabel];
+
+    // this.isExpandCategory[groupIndex] = !this.isExpandCategory[groupIndex];
+    group.selected = !group.selected;
+    // expand only selected parent dropdown category with that childs
+  }
+
+  optionClicked(name: string, index?: number) {
+    console.log('Selected option: ', name, 'having id: ', index);
+  }
+
+  toggleSelection(event: any, group: any) {
+    //console.log(group);
+    //console.log(event.checked);
+    let states = this.states.value;
+    states = states ? states : [];
+    if (event.checked) {
+      states.push(...group.options);
+    } else {
+      group.options.forEach((x: string) => states.splice(states.indexOf(x), 1));
+    }
+    this.states.setValue(states);
+    //console.log(states);
+    // console.log(this.states.value);
+    // here select all childs for this particular group
+  }
+
+  optionsGroups = [
+    {
+      groupLabel: 'A',
+      options: ['Alabama', 'Alaska', 'Arizona', 'Arkansas'],
+      selected: false
+    },
+    {
+      groupLabel: 'C',
+      options: ['California', 'Colorado', 'Connecticut'],
+      selected: false
+    },
+    {
+      groupLabel: 'D',
+      options: ['Delaware'],
+      selected: false
+    },
+    {
+      groupLabel: 'F',
+      options: ['Florida'],
+      selected: false
+    },
+    {
+      groupLabel: 'G',
+      options: ['Georgia'],
+      selected: false
+    },
+    {
+      groupLabel: 'H',
+      options: ['Hawaii'],
+      selected: false
+    },
+    {
+      groupLabel: 'I',
+      options: ['Idaho', 'Illinois', 'Indiana', 'Iowa'],
+      selected: false
+    },
+    {
+      groupLabel: 'K',
+      options: ['Kansas', 'Kentucky'],
+      selected: false
+    },
+    {
+      groupLabel: 'L',
+      options: ['Louisiana'],
+      selected: false
+    },
+    {
+      groupLabel: 'M',
+      options: [
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+      ],
+      selected: false
+    },
+    {
+      groupLabel: 'N',
+      options: [
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+      ],
+      selected: false
+    },
+    {
+      groupLabel: 'O',
+      options: ['Ohio', 'Oklahoma', 'Oregon'],
+      selected: false
+    },
+    {
+      groupLabel: 'P',
+      options: ['Pennsylvania'],
+      selected: false
+    },
+    {
+      groupLabel: 'R',
+      options: ['Rhode Island'],
+      selected: false
+    },
+    {
+      groupLabel: 'S',
+      options: ['South Carolina', 'South Dakota'],
+      selected: false
+    },
+    {
+      groupLabel: 'T',
+      options: ['Tennessee', 'Texas'],
+      selected: false
+    },
+    {
+      groupLabel: 'U',
+      options: ['Utah'],
+      selected: false
+    },
+    {
+      groupLabel: 'V',
+      options: ['Vermont', 'Virginia'],
+      selected: false
+    },
+    {
+      groupLabel: 'W',
+      options: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming'],
+      selected: false
+    },
+  ];
+  // #endregion
+
+
 
   constructor() { }
 
