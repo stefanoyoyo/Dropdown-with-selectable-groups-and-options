8dcb6b91a83b7ec115f5eaba8e224ecf3894093b
Stefano Amenta <ame@softechweb.it>
2022-11-22 09:45:10 +0100

    esempio tendina

--- src/app/components/select-options-groups/select-options-groups.component.ts
+++ src/app/components/select-options-groups/select-options-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select-options-groups',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-options-groups.component.scss']
 })
 export class SelectOptionsGroupsComponent implements OnInit {
+  isExpandCategory: boolean[] = [];
+  states = new FormControl();
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
   constructor() { }
 
